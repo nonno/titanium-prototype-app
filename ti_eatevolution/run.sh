72a8a55fd14ab40ce79dd32c15ced8647a6f72ef
Mauro Piccotti <m.piccotti@apra.it>
2015-10-03 11:23:41 +0200

    Correct run.sh script.

--- ti_eatevolution/run.sh
+++ ti_eatevolution/run.sh
@@ -1,7 +1,9 @@
-if [ "$1" != "" ]; then
-	appc ti build --platform $1 --log-level debug --device-id
+if [[ "$1" != ""] && ["$2" != "" ]]; then
+	appc ti build --platform $1 --log-level debug --target $2 --device-id
 else
 	echo Please enter platform: ios or android
 	read PLATFORM
-	appc ti build --platform $PLATFORM --log-level debug --device-id
+	echo Please enter the target: simulator-ios/emulator-android or device
+	read TARGET
+	appc ti build --platform $PLATFORM --log-level debug --target $TARGET --device-id
 fi
