2d41e1b04a0e9e7a960e27d738a831f0db3d143d
Mauro Piccotti <m.piccotti@apra.it>
2015-10-22 19:42:29 +0200

    Release version 0.5.

--- ti_eatevolution/deploy.sh
+++ ti_eatevolution/deploy.sh
@@ -5,7 +5,5 @@ ANDROID_OUTPUT_DIR=build/android/bin
 
 mkdir -p $OUTPUT_DIR
 
-#appc ti build --platform ios --build-only --force --log-level info --device-family ios --target dist-appstore --distribution-name "Mauro Piccotti" --pp-uuid d3f15939-2996-465e-92dc-70ca4dd68a67
-
 appc ti build --platform android --build-only --force --log-level info
 cp $ANDROID_OUTPUT_DIR"/Eat Evolution.apk" $OUTPUT_DIR"/EatEvolution.apk"
