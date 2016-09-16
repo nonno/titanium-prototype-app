a09a35def6952ee00d50bcaec0fb75c8742e549c
Mauro Piccotti <mauro.piccotti@gmail.com>
2016-09-16 19:11:17 +0200

    Add TestUI project for automated UI test with Xamarin.

---
+++ TestUI/TestUI/Tests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TestUI
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Repl();
+
+            Func<AppQuery, AppQuery> title = (arg) => arg.Marked("Eat Evolution");
+            Func<AppQuery, AppQuery> barFofoGarden = (arg) => arg.Marked("Bar Fofo Garden");
+            Func<AppQuery, AppQuery> filtersButton = (arg) => arg.Class("ActionMenuItemView").Marked("Filters");
+
+            // 1 - info page
+            app.WaitForElement(title);
+            app.Screenshot("1 - info page");
+            Assert.IsNotNull(app.Query("JOIN US OR RECOMMEND").First().Text);
+
+            app.TapCoordinates(100, 300);
+
+            // 2 - list
+            app.WaitForElement(barFofoGarden);
+            app.Screenshot("2 - list");
+
+            app.Tap(barFofoGarden);
+
+            // 3 - Fofo Garden detail
+            app.WaitForElement(barFofoGarden);
+            app.Screenshot("3 - Fofo Garden detail");
+            Assert.IsNotNull(app.Query("Bar Fofo Garden").First().Text);
+
+            app.TapCoordinates(100, 100);
+            app.WaitForElement(barFofoGarden);
+            app.Tap(filtersButton);
+
+            // 4 - filters screen
+            app.WaitForElement(x => x.Marked("Confirm"));
+            app.Screenshot("4 - filters screen");
+            Assert.IsNotNull(app.Query("Confirm").First().Text);
+            Assert.IsNotNull(app.Query("Cancel").First().Text);
+
+            app.Back();
+        }
+    }
+}
